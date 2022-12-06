a35cbae32d733aca2c7c5d04d1ff2fe46ecb068d
FireGhost1301 <88044725+FireGhost1301@users.noreply.github.com>
2022-12-07 00:21:27 +0700

    Update Dinic.h

--- content/graph/Dinic.h
+++ content/graph/Dinic.h
@@ -1,53 +1,76 @@
 /**
- * Author: chilli
- * Date: 2019-04-26
- * License: CC0
- * Source: https://cp-algorithms.com/graph/dinic.html
- * Description: Flow algorithm with complexity $O(VE\log U)$ where $U = \max |\text{cap}|$.
- * $O(\min(E^{1/2}, V^{2/3})E)$ if $U = 1$; $O(\sqrt{V}E)$ for bipartite matching.
- * Status: Tested on SPOJ FASTFLOW and SPOJ MATCHING, stress-tested
+ * Author: FireGhost
+ * Date: 2022-11-08
+ * Description: ez pz
  */
-#pragma once
 
-struct Dinic {
-  struct Edge {
-    int to, rev;
-    ll c, oc;
-    ll flow() { return max(oc - c, 0LL); } // if you need flows
-  };
-  vi lvl, ptr, q;
-  vector<vector<Edge>> adj;
-  Dinic(int n) : lvl(n), ptr(n), q(n), adj(n) {}
-  void addEdge(int a, int b, ll c, ll rcap = 0) {
-    adj[a].pb({b, sz(adj[b]), c, c});
-    adj[b].pb({a, sz(adj[a]) - 1, rcap, rcap});
-  }
-  ll dfs(int v, int t, ll f) {
-    if (v == t || !f) return f;
-    for (int& i = ptr[v]; i < sz(adj[v]); i++) {
-      Edge& e = adj[v][i];
-      if (lvl[e.to] == lvl[v] + 1)
-        if (ll p = dfs(e.to, t, min(f, e.c))) {
-          e.c -= p, adj[e.to][e.rev].c += p;
-          return p;
-        }
-    }
-    return 0;
-  }
-  ll calc(int s, int t) {
-    ll flow = 0; q[0] = s;
-    rep(L,0,31) do { // 'int L=30' maybe faster for random data
-      lvl = ptr = vi(sz(q));
-      int qi = 0, qe = lvl[s] = 1;
-      while (qi < qe && !lvl[t]) {
-        int v = q[qi++];
-        for (Edge e : adj[v])
-          if (!lvl[e.to] && e.c >> (30 - L))
-            q[qe++] = e.to, lvl[e.to] = lvl[v] + 1;
-      }
-      while (ll p = dfs(s, t, LLONG_MAX)) flow += p;
-    } while (lvl[t]);
-    return flow;
-  }
-  bool leftOfMinCut(int a) { return lvl[a] != 0; }
+// Usage:
+//   Dinitz dinic(n); // 0-based and 1-based
+//   dinic.addEdge(u, v, cap, reverseCap) // 1-direction and 2-direction
+//   dinic.getFlow(s, t) --> maxflow s->t
+// Time: O(V^2 E)
+struct Dinitz {
+	struct Edge {
+		int to, cap;
+		Edge(int to = 0, int cap = 0) : to(to), cap(cap) {}
+	};
+	int n, s, t;
+	vector<vector<int>> g;
+	vector<Edge> edge;
+	vector<int> dist, q;
+	vector<vector<int>::iterator> cur;
+
+	Dinitz(int n = 0) : n(n), g(n + 1), dist(n + 1), q(n + 1), cur(n + 1) {}
+	void addEdge(int u, int v, int cap, int reverseCap = 0) {
+		g[u].push_back(edge.size());
+		edge.emplace_back(v, cap);
+		g[v].push_back(edge.size());
+		edge.emplace_back(u, reverseCap);
+	}
+	int getFlow(int _s, int _t) {
+		s = _s, t = _t;
+		int flw = 0;
+		while (bfs()) {
+			for (int i = 0; i <= n; ++i)
+				cur[i] = g[i].begin();
+			flw += dfs(s, INT_MAX);
+		}
+		return flw;
+	}
+
+private:
+	bool bfs() {
+		int ql = 0, qr = 0;
+		dist.assign(n + 1, -1);
+		dist[s] = 0;
+		q[qr++] = s;
+		while (ql < qr && dist[t] == -1) {
+			int u = q[ql++];
+			for (int i : g[u]) {
+				int v = edge[i].to;
+				if (dist[v] == -1 && edge[i].cap) {
+					dist[v] = dist[u] + 1;
+					q[qr++] = v;
+				}
+			}
+		}
+		return dist[t] != -1;
+	}
+	int dfs(int u, int flowIn) {
+		if (u == t || !flowIn) return flowIn;
+
+		int flowOut = 0;
+		for (; cur[u] != g[u].end(); ++cur[u]) {
+			int i = *cur[u];
+			if (dist[edge[i].to] == dist[u] + 1 && edge[i].cap) {
+				int flw = dfs(edge[i].to, min(flowIn, edge[i].cap));
+				flowOut += flw;
+				flowIn -= flw;
+				edge[i].cap -= flw;
+				edge[i ^ 1].cap += flw;
+				if (!flowIn) break;
+			}
+		}
+		return flowOut;
+	}
 };
