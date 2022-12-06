2d751cee0b75850b4932a2d35b1a901d60931758
FireGhost1301 <88044725+FireGhost1301@users.noreply.github.com>
2022-12-07 00:22:35 +0700

    Update GomoryHu.h

--- content/graph/GomoryHu.h
+++ content/graph/GomoryHu.h
@@ -1,33 +1,64 @@
 /**
- * Author: chilli, Takanori MAEHARA
- * Date: 2020-04-03
- * License: CC0
- * Source: https://github.com/spaghetti-source/algorithm/blob/master/graph/gomory_hu_tree.cc#L102
- * Description: Given a list of edges representing an undirected flow graph,
- * returns edges of the Gomory-Hu tree. The max flow between any pair of
- * vertices is given by minimum edge weight along the Gomory-Hu tree path.
- * Time: $O(V)$ Flow Computations
- * Status: Tested on CERC 2015 J, stress-tested
- *
- * Details: The implementation used here is not actually the original
- * Gomory-Hu, but Gusfield's simplified version: "Very simple methods for all
- * pairs network flow analysis". PushRelabel is used here, but any flow
- * implementation that supports `leftOfMinCut` also works.
+ * Author: FireGhost
+ * Date: 2022-11-08
+ * Description: ez pz
  */
-#pragma once
 
-#include "PushRelabel.h"
+// Usage:
+//   GomoryHu g(n); // 0-based
+//   g.addEdge(u, v, c)  // 1-direction, using cap[u][v] += c
+//   g.calc()
+//   --> g.minCut[i][j] = min cut i-j
+//
+// Note:
+// - Used together with Dinic.h
+struct GomoryHu { // 0-based
+	vector<bool> ok;
+	vector<int> parent;
+	vector<vector<int>> cap, minCut;
+	int n;
+	Dinitz flow;
 
-typedef array<ll, 3> Edge;
-vector<Edge> gomoryHu(int N, vector<Edge> ed) {
-  vector<Edge> tree;
-  vi par(N);
-  rep(i,1,N) {
-    PushRelabel D(N); // Dinic also works
-    for (Edge t : ed) D.addEdge(t[0], t[1], t[2], t[2]);
-    tree.pb({i, par[i], D.calc(i, par[i])});
-    rep(j,i+1,N)
-      if (par[j] == par[i] && D.leftOfMinCut(j)) par[j] = i;
-  }
-  return tree;
-}
+	GomoryHu(int n = 0)
+		: n(n), flow(n), ok(n), parent(n, 0), cap(n, vector<int>(n, 0)), minCut(n, vector<int>(n, INT_MAX)) {}
+
+	void addEdge(int u, int v, int c) { // 1-direction
+		cap[u][v] += c;
+	}
+	void calc() {
+		for (int i = 1; i < n; ++i) {
+			flow = Dinitz(n); // reset Dinitz
+			for (int u = 0; u < n; ++u)
+				for (int v = 0; v < n; ++v)
+					if (cap[u][v]) flow.addEdge(u, v, cap[u][v]);
+
+			int mf = flow.getFlow(i, parent[i]);
+			bfs(i);
+			for (int j = i + 1; j < n; ++j)
+				if (ok[j] && parent[j] == parent[i])
+					parent[j] = i;
+
+			minCut[i][parent[i]] = minCut[parent[i]][i] = mf;
+			for (int j = 0; j < i; ++j)
+				minCut[i][j] = minCut[j][i] = min(mf, minCut[parent[i]][j]);
+		}
+	}
+
+private:
+	void bfs(int start) {
+		fill(ok.begin(), ok.end(), false);
+		queue<int> q;
+		q.push(start);
+		while (!q.empty()) {
+			int u = q.front();
+			q.pop();
+			for (int id : flow.g[u]) {
+				int v = flow.edge[id].to;
+				if (!ok[v] && flow.edge[id].cap) {
+					ok[v] = true;
+					q.push(v);
+				}
+			}
+		}
+	}
+};
